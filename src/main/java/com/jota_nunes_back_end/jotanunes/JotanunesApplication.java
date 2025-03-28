1dbab4248404031a7f51cd812acd07c11a03cd08
Ryan <ryannarelli12@gmail.com>
2025-03-27 22:37:33 -0300

    enviando arquivos iniciais

---
+++ src/main/java/com/jota_nunes_back_end/jotanunes/JotanunesApplication.java
@@ -0,0 +1,13 @@
+package com.jota_nunes_back_end.jotanunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotanunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotanunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jota_nunes_back_end/jotanunes/JotanunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jota_nunes_back_end.jotanunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotanunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
