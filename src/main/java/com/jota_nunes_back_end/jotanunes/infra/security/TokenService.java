2bc435a7196708bf5cc2c7b28239fc33aa963b95
Ryan <ryannarelli12@gmail.com>
2025-06-01 20:38:52 -0300

    fix: Adiconando id no jwt

--- src/main/java/com/jota_nunes_back_end/jotanunes/infra/security/TokenService.java
+++ src/main/java/com/jota_nunes_back_end/jotanunes/infra/security/TokenService.java
@@ -1,3 +1,54 @@
+//package com.jota_nunes_back_end.jotanunes.infra.security;
+//
+//import com.auth0.jwt.JWT;
+//import com.auth0.jwt.algorithms.Algorithm;
+//import com.auth0.jwt.exceptions.JWTCreationException;
+//import com.auth0.jwt.exceptions.JWTVerificationException;
+//import com.jota_nunes_back_end.jotanunes.models.UserAccount;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.Instant;
+//import java.time.LocalDateTime;
+//import java.time.ZoneOffset;
+//
+//@Service
+//public class TokenService {
+//    @Value("${api.security.token.secret}")
+//    private String secret;
+//
+//    public String generateToken(UserAccount userAccount){
+//        try{
+//            Algorithm algorithm = Algorithm.HMAC256(secret);
+//            String token = JWT.create()
+//                    .withIssuer("auth-api")
+//                    .withSubject(userAccount.getNumberRegister())
+//                    .withExpiresAt(genExpirationDate())
+//                    .sign(algorithm);
+//            return token;
+//        } catch (JWTCreationException exception) {
+//            throw new RuntimeException("Error while generating token", exception);
+//        }
+//    }
+//
+//    public String validateToken(String token){
+//        try {
+//            Algorithm algorithm = Algorithm.HMAC256(secret);
+//            return JWT.require(algorithm)
+//                    .withIssuer("auth-api")
+//                    .build()
+//                    .verify(token)
+//                    .getSubject();
+//        } catch (JWTVerificationException exception){
+//            return "";
+//        }
+//    }
+//
+//    private Instant genExpirationDate(){
+//        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+//    }
+//}
+
 package com.jota_nunes_back_end.jotanunes.infra.security;
 
 import com.auth0.jwt.JWT;
@@ -17,12 +68,13 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    public String generateToken(UserAccount userAccount){
-        try{
+    public String generateToken(UserAccount userAccount) {
+        try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
                     .withIssuer("auth-api")
-                    .withSubject(userAccount.getNumberRegister())
+                    .withSubject(userAccount.getNumberRegister()) // matrícula
+                    .withClaim("id", userAccount.getId())         // adiciona o ID do usuário
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
             return token;
@@ -31,20 +83,21 @@ public class TokenService {
         }
     }
 
-    public String validateToken(String token){
+    public String validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
                     .withIssuer("auth-api")
                     .build()
                     .verify(token)
-                    .getSubject();
-        } catch (JWTVerificationException exception){
+                    .getSubject(); // retorna a matrícula
+        } catch (JWTVerificationException exception) {
             return "";
         }
     }
 
-    private Instant genExpirationDate(){
+    private Instant genExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
+
