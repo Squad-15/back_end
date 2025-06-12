03ea10d0def4133254fd70e14e3bac592eaf7b73
Ryan <ryannarelli12@gmail.com>
2025-06-12 15:57:55 -0300

    Commit assinado teste

--- src/main/java/com/jota_nunes_back_end/jotanunes/configs/WebConfig.java
+++ src/main/java/com/jota_nunes_back_end/jotanunes/configs/WebConfig.java
@@ -5,6 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//public class WebConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins(
+//                                "http://localhost:3000", // Ambiente local
+//                                "https://jotanunestreinamento.netlify.app" // Domínio hospedado
+//                        )
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
+
 @Configuration
 public class WebConfig {
     @Bean
@@ -14,13 +34,15 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:3000", // Ambiente local
-                                "https://jotanunestreinamento.netlify.app" // Domínio hospedado
+                                "http://localhost:3000",
+                                "https://jotanunestreinamento.netlify.app"
                         )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
+                        .exposedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
 }
+
