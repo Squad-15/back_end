dc74eae49e2e4b05e4c0d0a12123217d00bc250f
Ryan <ryannarelli12@gmail.com>
2025-06-12 23:39:16 -0300

    enviando ajuste

--- src/main/java/com/jota_nunes_back_end/jotanunes/infra/security/SecurityConfigurations.java
+++ src/main/java/com/jota_nunes_back_end/jotanunes/infra/security/SecurityConfigurations.java
@@ -17,7 +17,7 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf().disable()
+                // .csrf().disable()
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         /*.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
